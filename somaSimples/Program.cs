b26c53d0517e671ae25cdc5e153e92a3478b396e
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-09-18 08:18:18 -0300

    primeiro commit

---
+++ somaSimples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a, b, SOMA;
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        SOMA = a + b;
+
+        Console.WriteLine("SOMA = " + SOMA);
+
+    }
+
+}
